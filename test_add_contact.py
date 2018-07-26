af34cef514d01bc1bffc129ee89c89413ee4b1b2
Vladimir Palamarchuk <vpalamarchuk@adyax.com>
2018-07-26 16:15:21 +0300

    Code refactoring - передача параметров в методы

--- test_add_contact.py
+++ test_add_contact.py
@@ -19,35 +19,35 @@ class test_add_contact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_contacs_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def create_contact(self, wd):
+    def create_contact(self, wd, firstname, middlename, lastname, title, country):
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("firstname")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("middlename")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("lastname")
+        wd.find_element_by_name("lastname").send_keys(lastname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("title")
+        wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("ukraine")
+        wd.find_element_by_name("address").send_keys(country)
         # submit contact form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -60,9 +60,18 @@ class test_add_contact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_contacs_page(wd)
-        self.create_contact(wd)
+        self.create_contact(wd, firstname="firstname", middlename="middlename", lastname="lastname", title="title", country="ukraine")
+        self.return_to_contact_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_contacs_page(wd)
+        self.create_contact(wd, firstname="", middlename="", lastname="", title="", country="")
         self.return_to_contact_page(wd)
         self.logout(wd)
 
