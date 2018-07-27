259be8909cb45850e743c4c1443acce37e547e73
Vladimir Palamarchuk <vpalamarchuk@adyax.com>
2018-07-27 13:19:56 +0300

    create_contact тест переведен на pytest с использованием фикстуры

--- application.py
+++ application.py
@@ -24,6 +24,7 @@ class Application:
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         wd.find_element_by_link_text("groups").click()
 
+    # GROUPS
     def open_groups_page(self):
         wd = self.wd
         # open groups page
@@ -50,6 +51,38 @@ class Application:
     def return_to_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
+
+    # CONTACTS
+    def open_contacts_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_contacts_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.country)
+        # submit contact form
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_contact_page()
+
+    def return_to_contact_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
 
     def logout(self):
         wd = self.wd
