8196826e4cc423f22d05dd9761687f782763203f
ak32pvi <vpalamarchuck@gmail.com>
2018-08-06 12:24:01 +0300

    Валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,13 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="firstname", middlename="middlename", lastname="lastname", title="title", country="ukraine"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", title="", country=""))
-    app.session.logout()
 
