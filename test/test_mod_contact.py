b3dcb28d9ebb968c089e45cc8e5d4736803f2d8b
ak32pvi <vpalamarchuck@gmail.com>
2018-08-07 17:51:03 +0300

    Homework #8 (preconditions)

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -3,8 +3,12 @@ from model.contact import Contact
 
 
 def test_modify_first_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
 
 
 def test_modify_first_contact_middlename(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.modify_first_contact(Contact(middlename="New middlename"))
