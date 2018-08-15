4448545bf78f269087ead363bfeeb0a05b9cde6f
ak32pvi <vpalamarchuck@gmail.com>
2018-08-15 15:38:22 +0300

    Проверка равенства объектов и списков (удаление)

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,10 @@ class Contact:
         self.lastname = lastname
         self.title = title
         self.country = country
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname="firstname", middlename="middlename", lastname="lastname", title="title", country="ukraine"))
+    app.contact.create(Contact(firstname="firstname", middlename="middlename", lastname="lastname", title="title", country="ourcountry"))
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
 
