0c2c0de736116d11a72dfe47517fee40df8b8615
Vladimir Palamarchuk <vpalamarchuk@adyax.com>
2018-08-27 19:30:51 +0300

    Отделение тестовых данных от сценариев

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", title="", address="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname", 10), company=random_string("company", 10),
-            title="title", address=random_string("address", 30),
-            homephone="12345", mobilephone="84633", workphone="123456", secondaryphone="09909", fax="77777",
-            email="abc@gmail.com", email2="support@gmail.com", email3="contact@gmail.com",
-            homepage="homepage field", byear="1988",
-            ayear="2018", address2="Earth", notes="Hello world!")
-    for i in range(5)
-]
+from data.add_contact import constant as testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
