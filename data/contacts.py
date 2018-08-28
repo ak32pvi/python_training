ec043ce85e7400cc5397e8542a85a02eafb370ba
ak32pvi <vpalamarchuck@gmail.com>
2018-08-28 13:43:20 +0300

    Ещё один способ параметризации тестов: динамическая генерация тестов (g / c)

---
+++ data/contacts.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+
+testdata = [
+
+    Contact(firstname="firstname1", lastname="lastname1", address="address1"),
+    Contact(firstname="firstname", lastname="lastname2", address="address2"),
+]
+
