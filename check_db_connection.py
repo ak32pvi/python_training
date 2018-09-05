d6563b8ff41c005a50a9ef3d82945ed48165b03c
ak32pvi <vpalamarchuck@gmail.com>
2018-09-05 14:11:44 +0300

    last changes (not working)

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,19 +1,28 @@
-import pymysql.cursors
+from fixture.orm import ORMFixture
+from model.group import Group
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+# try:
+#     l = db.get_group_list()
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass #db.destroy()
+
+# try:
+#     l = db.get_contacts_in_group(Group(id="32"))
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass #db.destroy()
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
-        print(row)
+    l = db.get_contacts_not_in_group(Group(id="32"))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    connection.close()
-
-try:
-    cursor = connection.cursor()
-    cursor.execute("select * from addressbook")
-    for row in cursor.fetchall():
-        print(row)
-finally:
-    connection.close()
+    pass #db.destroy()
